7e6abd2511bd17da322c39e58762e0c971e905ae
liugebenniao <liugebenniao@gmail.com>
2025-05-07 11:28:26 +0900

    Update my_utils.py

--- my_utils.py
+++ my_utils.py
@@ -20,11 +20,3 @@ def load_memory(file_path):
 def save_memory(file_path, data):
     with open(file_path, "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
-
-def is_active(start_hour, end_hour):
-    now = datetime.now()
-    hour = now.hour
-    if start_hour <= end_hour:
-        return start_hour <= hour < end_hour
-    else:
-        return hour >= start_hour or hour < end_hour
